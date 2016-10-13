d431be912bb8fbb412b7bd1589a90fb018b296f5
Andrew Farmer <ahfarmer@gmail.com>
2016-10-13 15:58:32 -0700

    style: write component css

--- src/component/Button.js
+++ src/component/Button.js
@@ -7,18 +7,31 @@ class Button extends React.Component {
   }
 
   render() {
+    let className = "component-button";
+    if (this.props.orange) {
+      className += " orange";
+    }
+    if (this.props.wide) {
+      className += " wide";
+    }
+
     return (
-      <button
-        className="component-button"
-        onClick={this.handleClick}
+      <div
+        className={className}
       >
-        {this.props.name}
-      </button>
+        <button
+          onClick={this.handleClick}
+        >
+          {this.props.name}
+        </button>
+      </div>
     );
   }
 }
 Button.propTypes = {
   name: React.PropTypes.string,
+  orange: React.PropTypes.bool,
+  wide: React.PropTypes.bool,
   clickHandler: React.PropTypes.func,
 };
 export default Button;
--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import Button from './Button';
+import React from 'react';
 import './ButtonPanel.css';
 
 class ButtonPanel extends React.Component {
@@ -14,30 +14,30 @@ class ButtonPanel extends React.Component {
           <Button name="AC" clickHandler={this.handleClick} />
           <Button name="+/-" clickHandler={this.handleClick} />
           <Button name="%" clickHandler={this.handleClick} />
-          <Button name="÷" clickHandler={this.handleClick} />
+          <Button name="÷" clickHandler={this.handleClick} orange />
         </div>
         <div>
           <Button name="7" clickHandler={this.handleClick} />
           <Button name="8" clickHandler={this.handleClick} />
           <Button name="9" clickHandler={this.handleClick} />
-          <Button name="x" clickHandler={this.handleClick} />
+          <Button name="x" clickHandler={this.handleClick} orange />
         </div>
         <div>
           <Button name="4" clickHandler={this.handleClick} />
           <Button name="5" clickHandler={this.handleClick} />
           <Button name="6" clickHandler={this.handleClick} />
-          <Button name="-" clickHandler={this.handleClick} />
+          <Button name="-" clickHandler={this.handleClick} orange />
         </div>
         <div>
           <Button name="1" clickHandler={this.handleClick} />
           <Button name="2" clickHandler={this.handleClick} />
           <Button name="3" clickHandler={this.handleClick} />
-          <Button name="+" clickHandler={this.handleClick} />
+          <Button name="+" clickHandler={this.handleClick} orange />
         </div>
         <div>
-          <Button name="0" clickHandler={this.handleClick} />
+          <Button name="0" clickHandler={this.handleClick} wide />
           <Button name="." clickHandler={this.handleClick} />
-          <Button name="=" clickHandler={this.handleClick} />
+          <Button name="=" clickHandler={this.handleClick} orange />
         </div>
       </div>
     );
