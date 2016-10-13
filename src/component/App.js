c7e46856f80d6d0681aad4c531c73ab30c3686e1
Andrew Farmer <ahfarmer@gmail.com>
2016-10-13 15:23:07 -0700

    remove unwanted files, create 'component' directory

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Welcome to App
+      </div>
+    );
+  }
+}
+export default App;
