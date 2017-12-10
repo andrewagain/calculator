e17adbffa82ac0662cdf7e09b76dd1b06ae099ba
Andrew Farmer <ahfarmer@gmail.com>
2017-12-10 09:39:33 -0800

    ↔️ match project structure to new cra projects

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -6,7 +6,7 @@ import isNumber from './isNumber';
  * calculator data object.
  *
  * Calculator data object contains:
- *   total:String      the running total
+ *   total:s      the running total
  *   next:String       the next number to be operated on with the total
  *   operation:String  +, -, etc.
  */
