c3c5978dc339b8879af5aeeb8419a4af7b5cb7e4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-01 13:26:42 -0500

    more tests

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -6,16 +6,45 @@ import { mount } from 'cypress-react-unit-test'
 import App from '../../src/component/App'
 import React from 'react'
 
+// a few utility page functions
+
+// individual numbers
+const press = caption => cy.contains('.component-button', caption).click()
+// function buttons
+const op = caption => cy.contains('button', caption).click()
+// result node
+const result = () => cy.get('.component-display')
+
 describe('App', () => {
   beforeEach(() => {
     mount(<App />)
   })
 
-  it('computes', () => {
+  it('multiplies', () => {
+    // the simplest test without any helper functions
     cy.contains('.component-button', '7').click()
     cy.contains('button', 'x').click()
     cy.contains('.component-button', '2').click()
     cy.contains('button', '=').click()
     cy.contains('.component-display', '14')
+  })
+
+  it('multiplies 2', () => {
+    // the simplest test without any helper functions
+    cy.contains('.component-button', '7').click()
+    cy.contains('button', 'x').click()
+    cy.contains('.component-button', '2').click()
+    cy.contains('button', '=').click()
+    cy.contains('.component-display', '14')
+  })
+
+  it('computes modulo', () => {
+    // lets use "page functions"
+    press('2')
+    press('5')
+    op('%')
+    press('1')
+    press('0')
+    result().contains('5')
   })
 })
