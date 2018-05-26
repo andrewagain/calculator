a19f37af855b39f4c0053eb1fe035891253bfa4e
Andrew Farmer <ahfarmer@gmail.com>
2018-05-26 15:13:31 -0700

    🛀 clean up tests

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -19,65 +19,94 @@ function expectButtons(buttons, expectation) {
   expect(pressButtons(buttons)).to.deep.equal(expectation);
 }
 
-it('should support 6', () => {
-  expectButtons(['6'], { next: '6' });
+function test(buttons, expectation) {
+  it(`buttons ${buttons.join(',')} -> ${JSON.stringify(expectation)}`, () => {
+    expectButtons(buttons, expectation);
+  })
+}
+
+test(['6'], { next: '6' });
+
+test(['6', '6'], { next: '66' });
+
+test(['6', '+', '6'], {
+  next: '6',
+  total: '6',
+  operation: '+',
 });
 
-it('should support 66', () => {
-  expectButtons(['6', '6'], { next: '66' });
+test(['6', '+', '6', '='], {
+  total: '12',
 });
 
-it('should support 6 + 6', () => {
-  expectButtons(['6', '+', '6'], {
-    next: '6',
-    total: '6',
-    operation: '+',
-  });
+test(['0', '0', '+', '0', '='], {
+  total: '0',
 });
 
-it('should support 6 + 6 =', () => {
-  expectButtons(['6', '+', '6', '='], {
-    total: '12',
-  });
+test(['6', '+', '6', '=', '9'], {
+  next: '9',
 });
 
-it('should support 00 + 0 =', () => {
-  expectButtons(['0', '0', '+', '0', '='], {
-    total: '0',
-  });
+test(['3', '+', '6', '=', '+'], {
+  total: '9',
+  operation: '+',
 });
 
-it('should support 6 + 6 = 9', () => {
-  expectButtons(['6', '+', '6', '=', '9'], {
-    next: '9',
-  });
+test(['3', '+', '6', '=', '+', '9'], {
+  total: '9',
+  operation: '+',
+  next: '9',
 });
 
-it('should support 3 + 6 = +', () => {
-  expectButtons(['3', '+', '6', '=', '+'], {
-    total: '9',
-    operation: '+',
-  });
+test(['3', '+', '6', '=', '+', '9', '='], {
+  total: '18',
 });
 
-it('should support 3 + 6 = + 9', () => {
-  expectButtons(['3', '+', '6', '=', '+', '9'], {
-    total: '9',
-    operation: '+',
-    next: '9',
-  });
+// When '=' is pressed and there is not enough information to complete
+// an operation, the '=' should be disregarded.
+test(['3', '+', '=', '3', '='], {
+  total: '6',
 });
 
-it('should support 3 + 6 = + 9 =', () => {
-  expectButtons(['3', '+', '6', '=', '+', '9', '='], {
-    total: '18',
-  });
+test(['+'], {
+  operation: '+',
 });
 
-it('should support 3 + = 3 =', () => {
-  // When '=' is pressed and there is not enough information to complete
-  // an operation, the '=' should be disregarded.
-  expectButtons(['3', '+', '=', '3', '='], {
-    total: '6',
-  });
+test(['+', '2'], {
+  next: '2',
+  operation: '+',
 });
+
+test(['+', '2', '+'], {
+  total: '2',
+  operation: '+',
+});
+
+test(['+', '2', '+', '+'], {
+  total: '2',
+  operation: '+',
+});
+
+test(['+', '2', '+', '5'], {
+  next: '5',
+  total: '2',
+  operation: '+',
+});
+
+test(['+', '2', '5'], {
+  next: '25',
+  operation: '+',
+});
+
+test(['+', '2', '5'], {
+  next: '25',
+  operation: '+',
+});
+
+test(['+', '6', '+', '5', '='], {
+  total: '11',
+});
+
+// should clear the operator when AC is pressed
+test(['1', '+', '2', 'AC'], {});
+test(['+', '2', 'AC'], {});
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,8 +1,8 @@
 import Big from 'big.js';
 
 export default function operate(numberOne, numberTwo, operation) {
-  const one = Big(numberOne);
-  const two = Big(numberTwo);
+  const one = Big(numberOne || '0');
+  const two = Big(numberTwo || '0');
   if (operation === '+') {
     return one.plus(two).toString();
   }
