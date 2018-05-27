5e7e6160033bd288c8da19c14407c84c2fa3ac74
Andrew Farmer <ahfarmer@gmail.com>
2018-05-27 15:05:16 -0700

    📋 add missing describe block

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -31,125 +31,127 @@ function test(buttons, expectation, only = false) {
   })
 }
 
-test(['6'], { next: '6' });
+describe('calculate', function() {
+  test(['6'], { next: '6' });
 
-test(['6', '6'], { next: '66' });
+  test(['6', '6'], { next: '66' });
 
-test(['6', '+', '6'], {
-  next: '6',
-  total: '6',
-  operation: '+',
-});
+  test(['6', '+', '6'], {
+    next: '6',
+    total: '6',
+    operation: '+',
+  });
 
-test(['6', '+', '6', '='], {
-  total: '12',
-});
+  test(['6', '+', '6', '='], {
+    total: '12',
+  });
 
-test(['0', '0', '+', '0', '='], {
-  total: '0',
-});
+  test(['0', '0', '+', '0', '='], {
+    total: '0',
+  });
 
-test(['6', '+', '6', '=', '9'], {
-  next: '9',
-});
+  test(['6', '+', '6', '=', '9'], {
+    next: '9',
+  });
 
-test(['3', '+', '6', '=', '+'], {
-  total: '9',
-  operation: '+',
-});
+  test(['3', '+', '6', '=', '+'], {
+    total: '9',
+    operation: '+',
+  });
 
-test(['3', '+', '6', '=', '+', '9'], {
-  total: '9',
-  operation: '+',
-  next: '9',
-});
+  test(['3', '+', '6', '=', '+', '9'], {
+    total: '9',
+    operation: '+',
+    next: '9',
+  });
 
-test(['3', '+', '6', '=', '+', '9', '='], {
-  total: '18',
-});
+  test(['3', '+', '6', '=', '+', '9', '='], {
+    total: '18',
+  });
 
-// When '=' is pressed and there is not enough information to complete
-// an operation, the '=' should be disregarded.
-test(['3', '+', '=', '3', '='], {
-  total: '6',
-});
+  // When '=' is pressed and there is not enough information to complete
+  // an operation, the '=' should be disregarded.
+  test(['3', '+', '=', '3', '='], {
+    total: '6',
+  });
 
-test(['+'], {
-  operation: '+',
-});
+  test(['+'], {
+    operation: '+',
+  });
 
-test(['+', '2'], {
-  next: '2',
-  operation: '+',
-});
+  test(['+', '2'], {
+    next: '2',
+    operation: '+',
+  });
 
-test(['+', '2', '+'], {
-  total: '2',
-  operation: '+',
-});
+  test(['+', '2', '+'], {
+    total: '2',
+    operation: '+',
+  });
 
-test(['+', '2', '+', '+'], {
-  total: '2',
-  operation: '+',
-});
+  test(['+', '2', '+', '+'], {
+    total: '2',
+    operation: '+',
+  });
 
-test(['+', '2', '+', '5'], {
-  next: '5',
-  total: '2',
-  operation: '+',
-});
+  test(['+', '2', '+', '5'], {
+    next: '5',
+    total: '2',
+    operation: '+',
+  });
 
-test(['+', '2', '5'], {
-  next: '25',
-  operation: '+',
-});
+  test(['+', '2', '5'], {
+    next: '25',
+    operation: '+',
+  });
 
-test(['+', '2', '5'], {
-  next: '25',
-  operation: '+',
-});
+  test(['+', '2', '5'], {
+    next: '25',
+    operation: '+',
+  });
 
-test(['+', '6', '+', '5', '='], {
-  total: '11',
-});
+  test(['+', '6', '+', '5', '='], {
+    total: '11',
+  });
 
-test(['0', '.', '4'], {
-  next: '0.4'
-});
+  test(['0', '.', '4'], {
+    next: '0.4'
+  });
 
-test(['.', '4'], {
-  next: '0.4'
-});
+  test(['.', '4'], {
+    next: '0.4'
+  });
 
-test(['.', '4', '-', '.', '2'], {
-  total: '0.4',
-  next: '0.2',
-  operation: '-'
-});
+  test(['.', '4', '-', '.', '2'], {
+    total: '0.4',
+    next: '0.2',
+    operation: '-'
+  });
 
-test(['.', '4', '-', '.', '2', '='], {
-  total: '0.2',
-});
+  test(['.', '4', '-', '.', '2', '='], {
+    total: '0.2',
+  });
 
-// should clear the operator when AC is pressed
-test(['1', '+', '2', 'AC'], {});
-test(['+', '2', 'AC'], {});
+  // should clear the operator when AC is pressed
+  test(['1', '+', '2', 'AC'], {});
+  test(['+', '2', 'AC'], {});
 
-test(['4', '%'], {
-  next: '0.04',
-});
+  test(['4', '%'], {
+    next: '0.04',
+  });
 
-test(['4', '%', 'x', '2', '='], {
-  total: '0.08',
-});
+  test(['4', '%', 'x', '2', '='], {
+    total: '0.08',
+  });
 
-test(['4', '%', 'x', '2'], {
-  total: '0.04',
-  operation: 'x',
-  next: '2',
-});
+  test(['4', '%', 'x', '2'], {
+    total: '0.04',
+    operation: 'x',
+    next: '2',
+  });
 
-// the percentage sign should also act as '='
-test(['2', 'x', '2', '%'], {
-  total: '0.04',
+  // the percentage sign should also act as '='
+  test(['2', 'x', '2', '%'], {
+    total: '0.04',
+  });
 });
