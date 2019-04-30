303f16072ede4d3830b50b955e62bb673cda0cde
itsaiub <itsaiub@gmail.com>
2019-04-30 10:46:10 +0600

    Button componet change into functional component

--- src/component/Button.js
+++ src/component/Button.js
@@ -2,25 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Button.css";
 
-class Button extends React.Component {
-  handleClick = () => {
-    this.props.clickHandler(this.props.name);
+const Button = ({ clickHandler, name, orange, wide }) => {
+  const handleClick = () => {
+    clickHandler(name);
   };
 
-  render() {
-    const className = [
-      "component-button",
-      this.props.orange ? "orange" : "",
-      this.props.wide ? "wide" : "",
-    ];
+  const className = [
+    "component-button",
+    orange ? "orange" : "",
+    wide ? "wide" : "",
+  ];
 
-    return (
-      <div className={className.join(" ").trim()}>
-        <button onClick={this.handleClick}>{this.props.name}</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={className.join(" ").trim()}>
+      <button onClick={handleClick}>{name}</button>
+    </div>
+  );
+};
 Button.propTypes = {
   name: PropTypes.string,
   orange: PropTypes.bool,
