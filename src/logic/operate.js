b74e2458dfc13de06e0bc26c81edb6f67399a3f4
Andrew Farmer <ahfarmer@gmail.com>
2019-05-17 11:02:47 -0700

    update all packages, react 16.8.6

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -13,8 +13,8 @@ export default function operate(numberOne, numberTwo, operation) {
     return one.times(two).toString();
   }
   if (operation === "÷") {
-    if (two == "0") {
-      alert('Divide by 0 error');
+    if (two === "0") {
+      alert("Divide by 0 error");
       return "0";
     } else {
       return one.div(two).toString();
