37b56077e78b82bf2088ec993d55becb47538de9
Alex Baker <alex37454@gmail.com>
2019-06-02 18:54:03 -0500

    Bug Fix for #17 Multiple Presses of Multiplication or Division Error (#29)
    * bug #17 fix
    
    * tests for fix for #17

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,7 +2,7 @@ import Big from "big.js";
 
 export default function operate(numberOne, numberTwo, operation) {
   const one = Big(numberOne || "0");
-  const two = Big(numberTwo || "0");
+  const two = Big(numberTwo || (operation === "÷" || operation === 'x' ? "1": "0")); //If dividing or multiplying, then 1 maintains current value in cases of null
   if (operation === "+") {
     return one.plus(two).toString();
   }
