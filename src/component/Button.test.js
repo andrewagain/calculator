b299c1448544b370a56c7a3d8b7820e34be9956c
Amy Kim <amesk3@gmail.com>
2019-07-15 20:39:40 -0400

    failing test

--- src/component/Button.test.js
+++ src/component/Button.test.js
@@ -6,7 +6,7 @@ import App from "./App";
 import ButtonPanel from "./ButtonPanel";
 import Button from "./Button";
 
-xtest("clicking button changes", () => {
+test("clicking button changes", () => {
   const wrapper = shallow(<Button clickHandler={ButtonPanel.clickHandler} />);
   wrapper.find("button").simulate("click");
   expect(container).not.toBeUndefined();
