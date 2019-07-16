56aa460d6b9b6cce99f7f27637b21af2659940aa
Omar De Leo <omar.deleo@gmail.com>
2019-07-15 20:26:53 -0400

    Test Travis CI

--- src/__tests__/isNumber.js
+++ src/__tests__/isNumber.js
@@ -1,9 +1,9 @@
 import isNumber from '../logic/isNumber';
 
 it('returns false when input is not a number', () => {
-    expect(isNumber('a')).toBe(false);
-});
+    expect(isNumber('a')).toBe(false)
+})
 
 it('returns false when input is a number', () => {
-    expect(isNumber(9)).toBe(true);
-});
+    expect(isNumber(9)).toBe(true)
+})
