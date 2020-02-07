0c414f57d04846222c7f4e0f20c25bd05602b758
Stefanos <stefanosu422@gmail.com>
2020-02-06 20:49:58 -0500

    updated tests for button components

---
+++ src/component/Button.test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from './Button'
+import { act } from "react-dom/test-utils";
+
---
+++ src/logic/operate.test.js
@@ -0,0 +1,7 @@
+import operate from './operate' 
+import chai from 'chai'
+
+const expect = chai.expect 
+
+
+expect(operate)
