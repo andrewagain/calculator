b3b48cc87d28fa849310ada43d9b418ea911b1a3
Denis Taka Uekita <uekita@gmail.com>
2020-05-21 04:17:27 -0300

    test: new screenshot

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -2,5 +2,6 @@ describe('Home page', function() {
     it('loads properly', function() {
         cy.visit('/');
         cy.get('body').happoScreenshot();
+        cy.contains('AC').happoScreenshot();
     });
 });
