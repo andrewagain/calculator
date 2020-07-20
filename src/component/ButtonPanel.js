f02d4e019cfa12f1ad0c8d8eeeb7ecad3124f04d
brindha.venkatesan <brindha.venkatesan@zohocorp.com>
2020-07-20 13:17:38 +0530

    ButtonPanel JSX Construction is optimised.

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -5,6 +5,14 @@ import PropTypes from "prop-types";
 import "./ButtonPanel.css";
 
 export default class ButtonPanel extends React.Component {
+  buttonRows = [
+    ["AC", "+/-", "%", "÷"],
+    ["7", "8", "9", "x"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="]
+  ]
+
   static propTypes = {
     clickHandler: PropTypes.func,
   };
@@ -12,39 +20,18 @@ export default class ButtonPanel extends React.Component {
   handleClick = buttonName => {
     this.props.clickHandler(buttonName);
   };
-
+  
+  getButtonRows(){
+    return this.buttonRows.map((buttons, index)=><div key={index}>{this.getButtons(buttons)}</div>);
+  }
+  getButtons(buttons){
+    let buttonLength = buttons.length;
+    return buttons.map((buttonName, index) => <Button name={buttonName} key={buttonName} clickHandler={this.handleClick} orange={index === buttonLength-1} wide={!index && buttonLength<4} /> )
+  }
   render() {
     return (
       <div className="component-button-panel">
-        <div>
-          <Button name="AC" clickHandler={this.handleClick} />
-          <Button name="+/-" clickHandler={this.handleClick} />
-          <Button name="%" clickHandler={this.handleClick} />
-          <Button name="÷" clickHandler={this.handleClick} orange />
-        </div>
-        <div>
-          <Button name="7" clickHandler={this.handleClick} />
-          <Button name="8" clickHandler={this.handleClick} />
-          <Button name="9" clickHandler={this.handleClick} />
-          <Button name="x" clickHandler={this.handleClick} orange />
-        </div>
-        <div>
-          <Button name="4" clickHandler={this.handleClick} />
-          <Button name="5" clickHandler={this.handleClick} />
-          <Button name="6" clickHandler={this.handleClick} />
-          <Button name="-" clickHandler={this.handleClick} orange />
-        </div>
-        <div>
-          <Button name="1" clickHandler={this.handleClick} />
-          <Button name="2" clickHandler={this.handleClick} />
-          <Button name="3" clickHandler={this.handleClick} />
-          <Button name="+" clickHandler={this.handleClick} orange />
-        </div>
-        <div>
-          <Button name="0" clickHandler={this.handleClick} wide />
-          <Button name="." clickHandler={this.handleClick} />
-          <Button name="=" clickHandler={this.handleClick} orange />
-        </div>
+          {this.getButtonRows()}
       </div>
     );
   }
