43d08b367ef570de237bf4e79145924eac84f50d
Gustav Sundin <sundin666@gmail.com>
2022-04-07 14:31:48 +0200

    Test multiplication

--- cypress/integration/basic_test.spec.ts
+++ cypress/integration/basic_test.spec.ts
@@ -2,4 +2,13 @@ describe("Basic tests", () => {
     it("should load page", () => {
         cy.visit("/");
     });
+
+    it("should multiply two numbers", () => {
+        cy.visit("/");
+        cy.get("button").contains("5").click();
+        cy.get("button").contains("x").click();
+        cy.get("button").contains("2").click();
+        cy.get("button").contains("=").click();
+        cy.get(".component-display").should("have.text", "10");
+    });
 });
