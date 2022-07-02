c44eda3e1d30c3427d39fec016c21ec89924d77e
Joel Mascarenhas <70596645+joelm-code@users.noreply.github.com>
2022-07-02 16:20:25 +0530

    initial commit

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,30 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 import "./Button.css";
 
-export default class Button extends React.Component {
-  static propTypes = {
-    name: PropTypes.string,
-    orange: PropTypes.bool,
-    wide: PropTypes.bool,
-    clickHandler: PropTypes.func,
-  };
+//Button component reacives all the destructured props
+export default function Button({ name, orange, wide, clickHandler }) {
+  const className = [
+    "component-button",
+    orange ? "orange" : "",
+    wide ? "wide" : ""
+  ];
 
-  handleClick = () => {
-    this.props.clickHandler(this.props.name);
-  };
-
-  render() {
-    const className = [
-      "component-button",
-      this.props.orange ? "orange" : "",
-      this.props.wide ? "wide" : "",
-    ];
-
-    return (
-      <div className={className.join(" ").trim()}>
-        <button onClick={this.handleClick}>{this.props.name}</button>
-      </div>
-    );
-  }
+  return (
+    <div className={className.join(" ").trim()}>
+      {/* The Class is Dynamically ^ named based on the props recived */}
+      {/* The onClick callsback clickhandler and send the props name back */}
+      <button onClick={() => clickHandler(name)}>{name}</button>
+    </div>
+  );
 }
